bf385873f1f4f73952b5ac32206e83bb21360631
Shameem <shameem.@keyvalue.systems>
2023-03-14 12:15:30 +0530

    add: day1 script

---
+++ day_1/script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if sudo docker pull golang:1.19-alpine3.17 && sudo docker pull alpine:3.17 && \
+   sudo docker images --filter=reference='golang:1.19-alpine3.17' --filter=reference='alpine:3.17'; then
+    echo "You are good to go for the devops workshop!"
+else
+    echo "An error occurred. Please connect with your workshop moderator."
+fi
+
